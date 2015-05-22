263f41ff06f4b0dc7dc90098fddb7314532ed34a
lijian <li_jian@gozap.com>
2015-05-22 21:30:53 +0800

    modify install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,11 @@ cd Python-3.3.0
 make
 make install
 ln -s /usr/local/python3/bin/python3 /usr/bin/python3
-wget http://api.ezhe.com/temp/dailyCheck.tgz
-tar xzf dailyCheck.tgz
+cd /home/
+wget -O dailyCheck.zip https://codeload.github.com/blackshadowwalker/dailyCheck/zip/develop
+unzip dailyCheck.zip
+mv dailyCheck-develop dailyCheck
 cd dailyCheck
 python3 ./dailyCheck.py > dailyCheck.log &
+ps aux | grep python
+
