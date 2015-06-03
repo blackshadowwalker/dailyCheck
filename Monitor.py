954b7de4c879cf70676694122bdee31101b2e02c
lijian <li_jian@gozap.com>
2015-06-03 20:29:27 +0800

    modify reg

--- Monitor.py
+++ Monitor.py
@@ -27,7 +27,7 @@ class Monitor(threading.Thread):
     def __checkTimeIsOk(self):
         hour = time.localtime(time.time())[3]
         min = time.localtime(time.time())[4]
-        if hour == 8 and min == 5:
+        if hour == 23 and min == 55:
             return True
         return False
 
@@ -40,8 +40,8 @@ class Monitor(threading.Thread):
                 print('.')
                 logging.info("logDir: %s" + self.logDir)
                 #昨天
-                localTime =  time.strftime('%Y-%m-%d',time.localtime(time.time() - 24*60*60))
-                regstr = '.*error.log.' + localTime + '.log'
+                yestday =  time.strftime('%Y-%m-%d',time.localtime(time.time() - 24*60*60))
+                regstr = '(.*error\.log\.' + yestday + '\.log)|(.*error\.log$)'
                 fileList = os.listdir(self.logDir)
                 fileList.sort()
                 logging.info("fileList: %s", fileList)
@@ -56,11 +56,11 @@ class Monitor(threading.Thread):
                 ip = socket.gethostbyname(hostname)
                 content = hostname + "/" + ip + "\r\n"
                 if len(self.logList) > 0:
-                    content += "昨天错误日志列表: \r\n"
+                    content += "错误日志列表: \r\n"
                     for file in self.logList:
                         content += file + "\r\n"
                 else:
-                    content += "暂无昨天错误日志，其他日志列表: \r\n"
+                    content += "暂无错误日志，其他日志列表: \r\n"
                     for file in fileList:
                         content += file + "\r\n"
                 self.email.send_mail(self.config.getAppName()+"-错误日志", content, self.logList)
